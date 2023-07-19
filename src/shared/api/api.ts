988af6c9152360956a0781c9dd651533819edf58
usercoderr <aidarali.prod@gmail.com>
2023-07-19 18:06:29 +0600

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,9 +1,14 @@
 import axios from 'axios';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage';
+import { config } from '@storybook/addon-actions';
 
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-    },
+});
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+    }
+    return config;
 });
